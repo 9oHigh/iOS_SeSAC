a394ed9df354e99712ca5657e1027b7f310393b7
9oHigh <wkdska957@naver.com>
2021-11-07 23:33:38 +0900

    FilmPromotion Proj : FeedBack version

--- FilmPromotionComitteeAPIProj/FilmPromotionComitteeAPIProj/ViewController.swift
+++ FilmPromotionComitteeAPIProj/FilmPromotionComitteeAPIProj/ViewController.swift
@@ -16,10 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var rankingTableView: UITableView!
     @IBOutlet var dateTextfield: UITextField!
     
+    //MARK: moviesData 및 Contains 제거하고 해보기
     let localRealm = try! Realm()
     var tasks : Results<DailyMovie>!
-    var moviesData : [MovieData] = []
-    var containDateList : [String] = []
+    var newDate : String = "20200401"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,91 +28,88 @@ class ViewController: UIViewController {
         rankingTableView.delegate = self
         rankingTableView.dataSource = self
         rankingTableView.backgroundColor = .black
+        
         //featuers
         dateTextfield.placeholder = "20200401"
-        //해당 컬럼의 값들 넣어주기.. 너무 수동적인데
-        containDateList = []
         
+        //Realm
         tasks = localRealm.objects(DailyMovie.self)
         print("위치 :",localRealm.configuration.fileURL!)
+        
+        //init
+        fetchData(dates: newDate)
     }
-    
-    @IBAction func searchButtonClicked(_ sender: UIButton) {
-        //가장 먼저
-        print(self.containDateList)
-        moviesData.removeAll()
-        if let text = dateTextfield.text{
-            if text == "" || text.count != 8{
-                let alert = UIAlertController(title: "오입력 안내", message: "년도/월/일 순으로 정확히 입력해주세요. ex)20201201", preferredStyle: .alert)
-                let action = UIAlertAction(title: "확인", style: .default, handler: nil)
-                alert.addAction(action)
-                
-                present(alert,animated: true,completion: nil)
-                dateTextfield.text = ""
-                return
-            }
-            // 데베에 들어가 있음
-            if self.containDateList.contains(text){
-                for item in localRealm.objects(DailyMovie.self).filter("inputDate == '\(text)'"){
-                    let name = item.name
-                    let date = item.date
-                    let data = MovieData(title: name, releaseDate: date)
-                    self.moviesData.append(data)
-                }
-                self.rankingTableView.reloadData()
-            } else {
-                // 안들어가 있음
-                // 여기서 DB에 저장필요
-                self.containDateList.append(text)
-                let url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\("key")&targetDt=" + text
-                
-                AF.request(url, method: .get).validate().responseJSON { response in
-                    switch response.result {
-                    case .success(let value):
-                        let json = JSON(value)
-                        
-                        for item in 0...9 {
-                            let title = json["boxOfficeResult"]["dailyBoxOfficeList"][item]["movieNm"].stringValue
-                            let date = json["boxOfficeResult"]["dailyBoxOfficeList"][item]["openDt"].stringValue
-                            let data = MovieData(title: title, releaseDate: date)
-                            let task = DailyMovie(name: title, date: date, inputDate: text)
-                            try! self.localRealm.write{
-                                self.localRealm.add(task)
-                            }
-                            self.moviesData.append(data)
-                        }
-                        self.rankingTableView.reloadData()
-                    case .failure(let error):
-                        print(error)
+    func fetchData(dates: String){
+        let url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\("key")&targetDt=\(dates)"
+        
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                for item in 0...9 {
+                    let title = json["boxOfficeResult"]["dailyBoxOfficeList"][item]["movieNm"].stringValue
+                    let date = json["boxOfficeResult"]["dailyBoxOfficeList"][item]["openDt"].stringValue
+                    let task = DailyMovie(name: title, date: date, inputDate: dates)
+                    try! self.localRealm.write{
+                        self.localRealm.add(task)
                     }
                 }
+                self.rankingTableView.reloadData()
+            case .failure(let error):
+                print(error)
             }
+        }
+    }
+    @IBAction func searchButtonClicked(_ sender: UIButton) {
+        
+        
+        //조금더 정확한 정규식이 필요할 것 같다.
+        //2004년부터 데이터 존재
+        let pattern : String = "^20([0-2])([0-9])([0-1])([0-9])([0-1])([0-9])$"
+        var text : String
+        
+        //정규식 활용
+        if let _ = dateTextfield.text?.range(of: pattern,options: .regularExpression) {
+            text = dateTextfield.text!
         } else {
-            let alert = UIAlertController(title: "오입력 안내", message: "년도/월/일 순으로 정확히 입력해주세요. ex)20201201", preferredStyle: .alert)
-            let action = UIAlertAction(title: "확인", style: .default, handler: nil)
-            alert.addAction(action)
-            
-            present(alert,animated: true,completion: nil)
+            //MARK: Extension을 이용하여 Alert표시
+            showAlert(alertText: "날짜", alertMessege: "날짜를 잘못 입력하셨습니다.", alertTitle: "확인")
+            dateTextfield.text = ""
+            return
+        }
+        
+        // 존재함
+        if !localRealm.objects(DailyMovie.self).filter("inputDate == '\(text)'").isEmpty{
+            newDate = text
+            self.rankingTableView.reloadData()
+        } else {
+            // 존재하지 않음
+            newDate = text
+            fetchData(dates: newDate)
         }
     }
 }
 extension ViewController : UITableViewDelegate,UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return moviesData.count
+        return 10
     }
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell") as? MovieTableViewCell else {
             return UITableViewCell()
-            
         }
-        cell.movieNameLabel.text = moviesData[indexPath.row].title
+        //MARK: localRealm에서 바로가지고오기
+        tasks = localRealm.objects(DailyMovie.self).filter("inputDate == '\(newDate)'")
+        
+        //가장처음 데이터가 없기 때문에 0에 접근해버린 상태로 진행된다. -> 오류남 // 예외처리를 해야함
+        guard let count = tasks?.count,indexPath.row < count else {return cell}
+        
+        cell.movieNameLabel.text = tasks[indexPath.row].name
         cell.movieNameLabel.font = UIFont.boldSystemFont(ofSize: 17)
         cell.movieNameLabel.textColor = .white
         
-        cell.dateLabel.text = moviesData[indexPath.row].releaseDate
+        cell.dateLabel.text = tasks[indexPath.row].date
         cell.dateLabel.textColor = .white
         
         cell.levelLabel.font = UIFont.boldSystemFont(ofSize: 17)
@@ -125,4 +122,13 @@ extension ViewController : UITableViewDelegate,UITableViewDataSource{
         return cell
     }
 }
-
+extension ViewController {
+    //Show Alert
+    func showAlert(alertText : String, alertMessege : String, alertTitle: String){
+        let alert = UIAlertController(title: alertText, message: alertMessege, preferredStyle: .alert)
+        let action = UIAlertAction(title: alertTitle, style: .default, handler: nil)
+        alert.addAction(action)
+        
+        self.present(alert,animated: true,completion: nil)
+    }
+}
